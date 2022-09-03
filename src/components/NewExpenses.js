2036c2d061616a53ed975106ee3b99dd970e6b30
AlejandroJGarcia <alegarcia42210@gmial.com>
2022-09-03 16:07:26 -0400

    add more components and css files

---
+++ src/components/NewExpenses.js
@@ -0,0 +1,21 @@
+import ExpenseItem from "./ExpenseItem";
+import "./NewExpenses.css";
+
+function NewExpenses(props) {
+  return (
+    <div className="expenses">
+      <ExpenseItem
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+        date={props.items[0].date}
+      />
+      <ExpenseItem
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+        date={props.items[1].date}
+      />
+    </div>
+  );
+}
+
+export default NewExpenses;
