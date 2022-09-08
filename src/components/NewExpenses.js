0f9ed7a36935beca15ac3a3cc3e8e3334ee40282
AlejandroJGarcia <alegarcia42210@gmial.com>
2022-09-07 21:29:49 -0400

    completed first project, added card component

--- src/components/NewExpenses.js
+++ src/components/NewExpenses.js
@@ -1,9 +1,10 @@
+import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./NewExpenses.css";
 
 function NewExpenses(props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -14,7 +15,17 @@ function NewExpenses(props) {
         amount={props.items[1].amount}
         date={props.items[1].date}
       />
-    </div>
+      <ExpenseItem
+        title={props.items[2].title}
+        amount={props.items[2].amount}
+        date={props.items[2].date}
+      />
+      <ExpenseItem
+        title={props.items[3].title}
+        amount={props.items[3].amount}
+        date={props.items[3].date}
+      />
+    </Card>
   );
 }
 
